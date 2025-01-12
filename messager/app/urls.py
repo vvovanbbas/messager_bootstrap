03ffdceb0c1ba3dbea85b48762221e878832e3c2
RiderHt <belmasvladimir10@gmail.com>
2025-01-12 18:31:26 +0300

    Сделали регистрацию, авторизацию, редактирование профиля.

---
+++ messager/app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('', index),
+    path('lk', lk),
+    path('ajaxReg', ajaxReg),
+    path('ajaxAuth', ajaxAuth)
+]
+
+
+
+
